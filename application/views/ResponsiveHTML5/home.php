3b07798ebb3b97122ba058c07dde10a324a40949
syahacbar <syahacbar@gmail.com>
2020-08-25 09:37:39 +0700

    Update home.php

--- application/views/ResponsiveHTML5/home.php
+++ application/views/ResponsiveHTML5/home.php
@@ -98,7 +98,10 @@
     <!-- ======= Counts Section ======= -->
     <section id="counts" class="counts">
       <div class="container">
-
+      <div class="section-title">
+          <h2>Jemaat</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+        </div>
         <div class="row">
 
           <div class="col-lg-2 col-md-6">
